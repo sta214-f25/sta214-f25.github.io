1e015bcb02fd1df3de2308f4632416a4b1f00bfe
Ciaran Evans <lughnasace@gmail.com>
2025-09-06 17:53:22 -0400

    drafting lecture 7

--- homework/logit_plot.R
+++ homework/logit_plot.R
@@ -1,8 +1,14 @@
 library(tidyverse)
 
-logodds_plot <- function(data, x, y, group = NULL,
-                         num_bins = 5, bin_method = "equal_size",
-                         reg_formula = y ~ x){
+titanic <- read.csv("https://sta214-f25.github.io/homework/Titanic.csv")
+
+logodds_plot <- function(data, x, y, group, transform,
+                         num_bins = 5, bin_method = "equal_size"){
+  
+  if(!missing(group)){
+    data <- data |>
+      mutate({{group}} := as.factor({{group}}))
+  }
   
   data |>
     select({{x}}, {{y}}, {{group}}) |>
@@ -12,21 +18,100 @@ logodds_plot <- function(data, x, y, group = NULL,
                      each=ceiling(n()/num_bins))[1:n()]) |>
     group_by(bin, {{group}}) |>
     summarize({{x}} := mean({{x}}),
-              prop = mean({{y}}),
-              num_obs = n()) |>
-    ungroup() #|>
-    # mutate(logodds = log(prop/(1 - prop))) |>
-    # ggplot(aes(x = {{x}},
-    #            y = logodds, 
-    #            color = as.factor({{group}}))) +
-    # geom_point()
+              prop = (sum({{y}}) + 0.5)/(n() + 1),
+              num_obs = n(),
+              .groups = "drop") |>
+    mutate(logodds = log(prop/(1 - prop))) |>
+    ggplot(aes(x = {{x}},
+               y = logodds,
+               color = {{group}})) +
+    geom_point() +
+    geom_smooth(se=F, method="lm", formula = reg_formula) +
+    theme_bw() +
+    labs(y = "Empirical log odds")
 }
 
 
-logodds_plot(titanic, Fare, Survived, Pclass) |>
-ggplot(aes(x = mean_x,
-           y = logodds)) +
-  geom_point()
+logodds_plot <- function(data, reg_formula, group,
+                         num_bins = 5){
+  
+  var_names <- all.vars(reg_formula)
+
+  if(length(var_names) != 2){
+    stop("Only one variable can appear on the x-axis")
+  }
+
+  if(!is.numeric(data[[var_names[2]]])){
+    stop("The explanatory variable must be numeric")
+  }
+  
+  # to do: error message if outcome is not binary
+  
+  data <- data |>
+    select(all_of(var_names), {{group}}) |>
+    drop_na()
+
+  dat <- model.frame(reg_formula, data)
+  
+
+  xname <- colnames(dat)[2]
+  yname <- colnames(dat)[1]
+  colnames(dat) <- c("y", "x")
+
+  if(any(is.infinite(dat$x))){
+    stop(paste("There are non-finite values in", xname))
+  }
+
+  if(!missing(group)){
+    dat <- dat |>
+      mutate({{group}} := as.factor(data |> pull({{group}})))
+  }
+
+  dat |>
+    arrange({{group}}, x) |>
+    group_by({{group}}) |>
+    mutate(bin = rep(1:num_bins,
+                     each=ceiling(n()/num_bins))[1:n()]) |>
+    group_by(bin, {{group}}) |>
+    summarize(x = mean(x),
+                prop = (sum(y) + 0.5)/(n() + 1),
+                num_obs = n(),
+                .groups = "drop") |>
+    mutate(logodds = log(prop/(1 - prop))) |>
+    ggplot(aes(x = x,
+               y = logodds,
+               color = {{group}})) +
+    geom_point() +
+    geom_smooth(se=F, method="lm", formula = y ~ x) +
+    theme_bw() +
+    labs(x = xname,
+         y = "Empirical log odds")
+}
+
+
+logodds_plot(titanic, Survived ~ log(Fare + 1), Pclass,
+             num_bins = 20)
+
+
+logodds_plot(titanic, Survived ~ log(Fare + 1), Pclass)
+
+logodds_plot(titanic, Survived ~ log(Fare), Pclass)
+
+logodds_plot(titanic, Survived ~ Age, 
+             Pclass,
+             num_bins = 15)
+
+
+
+logodds_plot(dengue, Dengue ~ Temperature, num_bins = 20)
+
+
+
+logodds_plot(titanic, log(Fare), Survived, Pclass,
+             num_bins = 20) +
+  facet_wrap(~Pclass)
+
+
 
 logodds_plot <- function(data, num_bins, bin_method,
                          x_name, y_name, grouping = NULL, 
